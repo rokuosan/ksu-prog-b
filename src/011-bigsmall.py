7a5d62aeb675ff132567fb2e4b34f126be842963
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-09-25 09:52:21 +0900

    課題1を作成

---
+++ src/011-bigsmall.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+# 第1回 課題1: bigsmall
+# 提出: https://cclms.kyoto-su.ac.jp/mod/assign/view.php?id=144514
+# 教材: https://ksubpb.github.io/2024/lesson01/assignments/#課題01-1-bigsmall
+
+import random
+
+num = random.randint(1, 10)  # 1 以上 10 以下の乱数を生成する．
+
+# num が 5 以上なら Big，5 未満なら Small を出力する．
+if num >= 5:
+    print("Big")
+else:
+    print("Small")
