b3a12c552e3b9ce87653ea6403fe20b42fd063bc
rokuosan <85651386+rokuosan@users.noreply.github.com>
2024-10-01 23:12:43 +0900

    階乗計算機

---
+++ src/04/factorial.py
@@ -0,0 +1,16 @@
+# 第4回 課題1: 階乗を計算する関数を作成する
+# 提出: https://cclms.kyoto-su.ac.jp/mod/assign/view.php?id=
+# 教材: https://ksubpb.github.io/2024/lesson04/assignments/#課題04-1-階乗を計算する関数を作成する
+
+import sys
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+for arg in sys.argv[1:]:
+    n = int(arg)
+    print(f"{n}! = {factorial(n)}")
