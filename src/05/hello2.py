3f0794ec2621b80cd2b1adc11b52268aa287f667
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-10-30 10:10:50 +0900

    5回目の提出課題で増えていた分を書いた

---
+++ src/05/hello2.py
@@ -0,0 +1,16 @@
+# 第5回 課題1: デフォルト引数を持つ Hello World
+# 提出: https://cclms.kyoto-su.ac.jp/mod/assign/view.php?id=
+# 教材: https://ksubpb.github.io/2024/lesson04/assignments/#課題05-1-デフォルト引数を持つ-hello-world
+
+def hello(text: str = None) -> str:
+    if text is None:
+        return "Hello World"
+    elif text == "Python":
+        return "Hi! Python"
+    else:
+        return f"Hello {text}".strip()
+
+
+assert hello() == "Hello World",          "Error: hello() が 'Hello World' を返しません．"
+assert hello("Python") == "Hi! Python", "Error: hello('Python') が 'Hi Python' を返しません．"
+assert hello("Tamada") == "Hello Tamada",     "Error: hello('Tamada') が 'Hello Tamada' を返しません．"
---
+++ src/05/hello3.py
@@ -0,0 +1,16 @@
+# 第5回 課題2: 引数により挙動が変わる Hello World
+# 提出: https://cclms.kyoto-su.ac.jp/mod/assign/view.php?id=
+# 教材: https://ksubpb.github.io/2024/lesson04/assignments/#課題05-1-デフォルト引数を持つ-hello-world
+
+def hello(text: str = None) -> str:
+    if text is None:
+        return "Hello World"
+    elif text == "World":
+        return "Hi! World"
+    else:
+        return f"Hello {text}".strip()
+
+
+assert hello() == "Hello World",          "Error: hello() が 'Hello World' を返しません．"
+assert hello("Python") == "Hello Python", "Error: hello('Python') が 'Hello Python' を返しません．"
+assert hello("World") == "Hi! World",     "Error: hello('World') が 'Hi! World' を返しません．"
