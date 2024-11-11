862014d6b40c7298242a964014c90299b7586b51
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2024-11-11 11:21:24 +0900

    増えていた課題6-1 コンビネーションを追加

---
+++ src/06/combination.py
@@ -0,0 +1,12 @@
+import sys
+
+def combination(n, k):
+    if k == 0 or k == n:
+        return 1
+    else:
+        return  combination(n-1, k) + combination(n-1, k-1)
+
+n = int(sys.argv[1])
+k = int(sys.argv[2])
+
+print(f'combination({n}, {k}) = {combination(n, k)}')
